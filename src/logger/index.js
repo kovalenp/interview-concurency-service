4b52fca12e56944bf2dfd84b5cb07b7164d7b20c
Pavel Kovalenko <pavel@kovalenko.cz>
2020-04-12 23:48:14 +0200

    Added basic logger module

---
+++ src/logger/index.js
@@ -0,0 +1,16 @@
+const setLogLevel = () => {
+  // couple LOG_LEVEL to ENV
+  // supress logs for local unit tests
+  switch (process.env.CONFIG_ENV) {
+    case 'production':
+      return 'info'
+    case 'development':
+      return 'trace'
+    case 'local-test':
+      return 'silent'
+    default:
+      return 'debug'
+  }
+}
+
+module.exports = require('pino')({ level: setLogLevel() })
